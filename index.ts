fa7ddbd3b4efbcfbbf07b8b723ba79fe4dfedaac
Yehhmii <franciscoabhulimen@gmail.com>
2025-01-14 18:47:28 -0800

    started my typescript journey

---
+++ index.ts
@@ -0,0 +1,21 @@
+// Type inferrance
+let chef = 'Ade'
+let pans = 7
+let canCook = true
+
+chef = "Mike"
+pans = 10
+canCook = false
+
+// Type annotation
+let planet: string = 'Earth'
+let moon: number = 1
+let isRound: boolean = true
+
+planet = 'Mars'
+moon = 12
+isRound = false 
+
+// null and undefined 
+let something: null
+let anotherSomething: undefined   //these two can't be changed
